a7ca0e1d163adb5e7e500632114b7621fa1f8441
HillaryWebb-coder <hillariosky@gmail.com>
2022-06-10 15:20:16 +0100

    Created thr rock, paper, scissors project

---
+++ main.py
@@ -0,0 +1,44 @@
+from random import choice
+
+choices = ["R", "P", "S"]
+
+choicesDict = {
+    "R": "Rock",
+    "P": "Paper",
+    "S": "Scissors"
+}
+
+print("*****************ROCK PAPER SCISSORS****************")
+print("choices: R(Rock), P(Paper), S(Scissors)")
+print("press q to quit")
+
+
+def program(userChoice: str, cpuChoice: str) -> str:
+    if userChoice.isnumeric():
+        return "Please enter a valid choice"
+    elif userChoice == cpuChoice:
+        return "It's a tie"
+    elif userChoice == "R":
+        return "You win" if cpuChoice == "S" else "You Lose"
+    elif userChoice == "P":
+        return "You win" if cpuChoice == "R" else "You Lose"
+    elif userChoice == "S":
+        return "You win" if cpuChoice == "P" else "You Lose"
+    else:
+        return "please enter a valid choice"
+
+
+while True:
+    userChoice = input("Make a choice: ")
+    userChoice = userChoice.upper()
+    cpuChoice = choice(choices)
+
+    if userChoice.lower() == "q":
+        break
+    elif not userChoice:
+        print("Please enter a value")
+        continue
+
+    print("user: {} :: cpu: {}".format(
+        choicesDict[userChoice], choicesDict[cpuChoice]))
+    print(program(userChoice=userChoice, cpuChoice=cpuChoice))
